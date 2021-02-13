80cf548a37fd25c1df5265e29e52e31febf8c3b9
lekekbot <alim97460@gmail.com>
2021-02-13 15:46:43 +0800

    1.1.1

--- js/function.js
+++ js/function.js
@@ -40,9 +40,40 @@ $(document).ready(function () {
     });
 });
 
+// about tabs
+$(document).ready(function () {
+    $('a[href*=#]').bind('click', function (e) {
+        e.preventDefault(); // prevent hard jump, the default behavior
+
+        var target = $(this).attr("href"); // Set the target as variable
+
+        // perform animated scrolling by getting top-position of target-element and set it as scroll target
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 600, function () {
+            location.hash = target; //attach the hash (#jumptarget) to the pageurl
+        });
+        return false;
+    });
+});
+
+$(window).scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+    // Assign active class to nav links while scolling
+    $('.page-section').each(function (i) {
+        if ($(this).position().top - 100 <= scrollDistance + 100) {
+            $('.tabs').removeClass('active');
+            $('.tabs').eq(i).addClass('active');
+        }
+    });
+}).scroll();
+
+
+
+
 window.onbeforeunload = function (e) {
     window.onunload = function () {
-            window.localStorage.clear()
+        window.localStorage.clear()
     }
     return undefined;
 };

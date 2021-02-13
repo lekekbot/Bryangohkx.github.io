eefebdb73fa5ad2d3ecb902fef3e38e7858b5a88
lekekbot <alim97460@gmail.com>
2021-02-13 18:16:58 +0800

    v1.1.2

--- js/function.js
+++ js/function.js
@@ -41,27 +41,27 @@ $(document).ready(function () {
 });
 
 // about tabs
-$(document).ready(function () {
-    $('a[href*=#]').bind('click', function (e) {
-        e.preventDefault(); // prevent hard jump, the default behavior
+$('a[href*=#]').bind('click', function (e) {
+    e.preventDefault(); // prevent hard jump, the default behavior
 
-        var target = $(this).attr("href"); // Set the target as variable
+    var target = $(this).attr("href"); // Set the target as variable
 
-        // perform animated scrolling by getting top-position of target-element and set it as scroll target
-        $('html, body').stop().animate({
-            scrollTop: $(target).offset().top
-        }, 600, function () {
-            location.hash = target; //attach the hash (#jumptarget) to the pageurl
-        });
-        return false;
+    // perform animated scrolling by getting top-position of target-element and set it as scroll target
+    var scrollToPosition = $(target).offset().top - 58;
+    $('html').animate({
+        scrollTop: scrollToPosition
+        }, 50, function () {
+        location.hash = target; //attach the hash (#jumptarget) to the pageurl
     });
+    return false;
 });
+
 
 $(window).scroll(function () {
     var scrollDistance = $(window).scrollTop();
     // Assign active class to nav links while scolling
     $('.page-section').each(function (i) {
-        if ($(this).position().top - 100 <= scrollDistance + 100) {
+        if ($(this).offset().top -60 <= scrollDistance) {
             $('.tabs').removeClass('active');
             $('.tabs').eq(i).addClass('active');
         }
